7729c8ebbc8b6e2b65e0b0a5d92bcd8e595b4776
cmomacpro <xujijun@jd.com>
2018-06-08 19:12:50 +0800

    init
    Signed-off-by: cmomacpro <xujijun@jd.com>

---
+++ Example/Pods/Target Support Files/JDPSTest/JDPSTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JDPSTest : NSObject
+@end
+@implementation PodsDummy_JDPSTest
+@end
---
+++ Example/Pods/Target Support Files/Pods-JDPSTest_Tests/Pods-JDPSTest_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JDPSTest_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JDPSTest_Tests
+@end
